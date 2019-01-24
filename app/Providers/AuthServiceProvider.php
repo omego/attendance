55c2c8a569ed110a1344c7e8cb5ce7ff8b849480
Ssy3 <ssy.vip@gmail.com>
2019-01-24 16:18:44 +0300

    Global Admin True

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,5 +28,11 @@ class AuthServiceProvider extends ServiceProvider
 
         //
         Passport::routes();
+
+        // Implicitly grant "Admin" role all permissions
+      // This works in the app by using gate-related functions like auth()->user->can() and @can()
+      Gate::before(function ($user, $ability) {
+          return $user->hasRole('admin');
+      });
     }
 }
