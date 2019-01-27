8d999c05ad7d017a6d76ebbc6e23eebe223111e9
omego <mserihi@gmail.com>
2019-01-27 21:34:50 +0300

    backup init

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,7 +47,10 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
-
+        'dropbox' => [
+            'driver' => 'dropbox',
+            'authorizationToken' => env('DROPBOX_ACCESS_TOKEN'),
+       ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
