1454d20423c21163aeb231b03be3b8d2b896af00
ManalNawar <manal.s.nawar@gmail.com>
2019-12-17 09:30:17 +0300

    add college to block

--- app/Block.php
+++ app/Block.php
@@ -13,6 +13,11 @@ class Block extends Model
         return $this->belongsTo('App\Group');
     }
 
+    public function college()
+    {
+        return $this->belongsTo('App\College');
+    }
+
     public function user()
     {
         return $this->belongsToMany('App\User','block_user', 'block_id','user_id');
