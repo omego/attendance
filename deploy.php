43194c39c6993e4670a4bd2a6775f25d99da33cb
omego <mserihi@gmail.com>
2020-11-15 12:17:37 +0300

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -57,5 +57,5 @@ after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-// before('deploy:symlink', 'artisan:migrate');
+before('deploy:symlink', 'artisan:migrate');
 
